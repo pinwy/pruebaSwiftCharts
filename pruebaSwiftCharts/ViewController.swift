e74d8b6f5d30784f323987f27af863f66fd5b1f3
Luis Octavio Samaniego Barajas <luis.samaniego86@gmail.com>
2016-09-05 23:37:02 -0500

    Initial Commit

---
+++ pruebaSwiftCharts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaSwiftCharts
+//
+//  Created by Luis Octavio Samaniego Barajas on 05/09/16.
+//  Copyright © 2016 Luis Octavio Samaniego Barajas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
