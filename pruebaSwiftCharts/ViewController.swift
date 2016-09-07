4028068c389451ffddd6d26d959c68a1319b55f0
Luis Octavio Samaniego Barajas <luis.samaniego86@gmail.com>
2016-09-06 23:11:47 -0500

    Se pinta una gráfica pero le faltan todos los indicadores de valores, se sube a manera de respaldo, por que hay un tema de vistas que intentaré resolver

--- pruebaSwiftCharts/ViewController.swift
+++ pruebaSwiftCharts/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import SwiftCharts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        crearGrafica()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    func crearGrafica(){
+        let chartConfig = BarsChartConfig(
+            valsAxisConfig: ChartAxisConfig(from: 0, to: 8, by: 2)
+        )
+        
+        let chart = BarsChart(
+            frame: CGRectMake(0, 70, 300, 500),
+            chartConfig: chartConfig,
+            xTitle: "X axis",
+            yTitle: "Y axis",
+            bars: [
+                ("A", 2),
+                ("B", 4.5),
+                ("C", 3),
+                ("D", 5.4),
+                ("E", 6.8),
+                ("F", 0.5)
+            ],
+            color: UIColor.redColor(),
+            barWidth: 20
+        )
+        
+        self.view.addSubview(chart.view)
+        //self.chart = chart
 
+    }
 }
 
